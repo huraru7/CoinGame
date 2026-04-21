a091eab6ca41fa2ea51a4f566510b4d5645fe7e3
huraru <hurarunium@gmail.com>
2026-04-21 13:17:53 +0900

    [Updata]プッシャー上のコインがプッシャーと一緒に動くように

--- Assets/sprict/Pusher.cs
+++ Assets/sprict/Pusher.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Pusher : MonoBehaviour
 {
     private Vector3 Initialposition;
+    private Rigidbody rb;
 
-    public bool pusherSwitch;
+    public bool pusherSwitch = true;
     public float amplitude = 1f;
     public float speed = 1f;
 
     void Start()
     {
         Initialposition = transform.position;
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (pusherSwitch)
         {
-            float z = Initialposition.z + Mathf.Sin(Time.time * speed) * amplitude;
-            transform.position = new Vector3(Initialposition.x, Initialposition.y, z);
+            float z = Initialposition.z + Mathf.Sin(Time.fixedTime * speed) * amplitude;
+            rb.MovePosition(new Vector3(Initialposition.x, Initialposition.y, z));
         }
     }
 }
